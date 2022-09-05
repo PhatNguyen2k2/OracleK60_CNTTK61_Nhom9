a43ae066708744d608b6fb2d19d42db6ad40bfbe
DESKTOP-DN5FP91\ASIA <anhtrankoc@gmail.com>
2022-09-05 07:19:20 +0700

    update server

---
+++ server/scoreResult/src/main/java/com/viewresult/scoreResult/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.viewresult.scoreResult.controller;
+
+import java.util.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.viewresult.scoreResult.model.Student;
+
+
+@RestController
+@RequestMapping("/api/v1")
+
+public class StudentController {
+
+}
--- server/scoreResult/src/main/java/com/viewresult/scoreResult/model/Student.java
+++ server/scoreResult/src/main/java/com/viewresult/scoreResult/model/Student.java
@@ -7,17 +7,18 @@ import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "DATA")
 public class Student {
 	@Id
 	private String id;
 	@NonNull
-	private String name;
-	private String gender;
 	private float maths;
 	private float literatures;
 	private float foreignLang;
@@ -27,72 +28,74 @@ public class Student {
 	private float history;
 	private float geography;
 	private float civicEdu;
+	
+	@Column(name = "SBD", nullable = false)
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
+	@Column(name = "Toán", nullable = false)
 	public float getMaths() {
 		return maths;
 	}
 	public void setMaths(float maths) {
 		this.maths = maths;
 	}
+	@Column(name = "Ngữ văn", nullable = false)
 	public float getLiteratures() {
 		return literatures;
 	}
 	public void setLiteratures(float literatures) {
 		this.literatures = literatures;
 	}
+	@Column(name = "Tiếng Anh", nullable = false)
 	public float getForeignLang() {
 		return foreignLang;
 	}
 	public void setForeignLang(float foreignLang) {
 		this.foreignLang = foreignLang;
 	}
+	@Column(name = "Vật lí", nullable = false)
 	public float getPhysics() {
 		return physics;
 	}
 	public void setPhysics(float physics) {
 		this.physics = physics;
 	}
+	
+	@Column(name = "Hóa học", nullable = false)
 	public float getChemistry() {
 		return chemistry;
 	}
 	public void setChemistry(float chemistry) {
 		this.chemistry = chemistry;
 	}
+	@Column(name = "Sinh học", nullable = false)
 	public float getBiology() {
 		return biology;
 	}
 	public void setBiology(float biology) {
 		this.biology = biology;
 	}
+	
+	@Column(name = "Lịch sử", nullable = false)
 	public float getHistory() {
 		return history;
 	}
 	public void setHistory(float history) {
 		this.history = history;
 	}
+	@Column(name = "Địa lí", nullable = false)
 	public float getGeography() {
 		return geography;
 	}
 	public void setGeography(float geography) {
 		this.geography = geography;
 	}
+	
+	@Column(name = "GDCD", nullable = false)
 	public float getCivicEdu() {
 		return civicEdu;
 	}
