8df888b7fb994c788b17d3415479aee631a58441
Dungpi <phamdung.080602@gmail.com>
2022-09-12 16:40:58 +0700

    Them captcha nhung van loi

---
+++ client/src/components/form/Captcha/Captcha.jsx
@@ -0,0 +1,48 @@
+import { ReCaptcha } from 'react-recaptcha-google'
+class ExampleComponent extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
+    this.verifyCallback = this.verifyCallback.bind(this);
+  }  
+  
+  componentDidMount() {
+    if (this.captchaDemo) {
+        console.log("started, just a second...")
+        this.captchaDemo.reset();
+    }
+  }  
+  
+  onLoadRecaptcha() {
+      if (this.captchaDemo) {
+          this.captchaDemo.reset();
+      }
+  }  
+  
+  verifyCallback(recaptchaToken) {
+    // Here you will get the final recaptchaToken!!!  
+    console.log(recaptchaToken, "<= your recaptcha token")
+  }  
+  
+  render() {
+    return (
+      <div>
+        {/* You can replace captchaDemo with any ref word */}
+        <ReCaptcha
+            ref={(el) => {this.captchaDemo = el;}}
+            size="normal"
+            data-theme="dark"            
+            render="explicit"
+            sitekey="<ĐIỀN SITE KEY VÀO ĐÂY>"
+            onloadCallback={this.onLoadRecaptcha}
+            verifyCallback={this.verifyCallback}
+        />
+        <code>
+          1. Add <strong>your site key</strong> in the ReCaptcha component. <br/>
+          2. Check <strong>console</strong> to see the token.
+        </code>
+      </div>
+    );
+  };
+};
+export default ExampleComponent;
--- client/src/components/form/form.jsx
+++ client/src/components/form/form.jsx
@@ -1,14 +1,32 @@
+import React from 'react';
+// import ExampleComponent from './Captcha';
+import './form.scss';
 function Form() {
   return (
-    <div>
-      <h4>This is form component</h4>
-      <img
-        src="https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/286613266_1057744294874069_2985985515345412091_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_ohc=_dMsxGAhA4sAX-JJOlT&_nc_ht=scontent.fhan14-2.fna&oh=03_AVIIlrMcb7nvBqOkD9-4Fc-S1qgIjX3z_I2HRnYuwhVGTg&oe=633BE53F"
-        alt="ehe"
-        width="200"
-        height="250"
-      />
-      <p>This is Dunggdung</p>
+    <div className="content">
+      <form className="form-content" id="Form">
+        <div className="form-title">
+          <h3>SỞ GIÁO DỤC VÀ ĐÀO TẠO HÀ NỘI</h3>
+          <h2>TRA CỨU ĐIỂM THI THPT QUỐC GIA 2018</h2>
+        </div>
+        <div className="form-input">
+          <div className="input-guide">
+            <p>Thí Sinh Nhập Số Báo Danh Và Mã Xác Nhận Vào Các Ô Dưới Đây</p>
+          </div>
+          <div className="input-group">
+            <label>Số báo danh</label>
+            <input type="text" />
+          </div>
+          <div className="input-group">
+            <label>Mã Xác Nhận</label>
+            <input type="text" />
+          </div>
+        </div>
+        <div className="form-button">
+          <button>Tra Cứu</button>
+        </div>
+          {/* <ExampleComponent /> */}
+      </form>
     </div>
   );
 }
