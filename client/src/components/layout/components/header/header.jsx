f09877fe39f41d3497a8e27b13dff19ce0b24cc0
vychuho.201002@gmail.com <vychuho.201002@gmail.com>
2022-09-13 22:54:52 +0700

    lam xong

--- client/src/components/layout/components/header/header.jsx
+++ client/src/components/layout/components/header/header.jsx
@@ -1,9 +1,28 @@
-function Header() {
+// function Header() {
+//   return (
+//     <header>
+//       <h2>This is header</h2>
+//     </header>
+//   );
+// }
+import React from "react";
+import './header.scss';
+import logo from '../images/logo-removebg-preview (1).png';
+function Header(){
   return (
-    <header>
-      <h2>This is header</h2>
-    </header>
-  );
-}
+    <div className="nav-bg">
+      <div className="container">
+        <div className="nav">
+          <div className="nav-logo">
+            <img src={logo}/>
+          </div>
 
+          <div className="nav-content">
+            <p>SỞ GIÁO DỤC VÀ ĐÀO TẠO THÀNH PHỐ HỒ CHÍ MINH <br /> TRANG THÔNG TIN KỲ THI TỐT NGHIỆP THPT </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default Header;
