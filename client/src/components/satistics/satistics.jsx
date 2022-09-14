c15177a63f5b4506313a4609ec22d43af8127b1e
minhty123 <minhty1456.com>
2022-09-14 23:25:35 +0700

    runok

--- client/src/components/satistics/satistics.jsx
+++ client/src/components/satistics/satistics.jsx
@@ -2,37 +2,82 @@ import styles from './satistics.scss';
 
 import React from 'react';
 
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js';
-
 import { Bar } from 'react-chartjs-2';
 
+import { Chart as ChartJS } from 'chart.js/auto';
+
 function Statistics() {
-  <div>
-    <Bar
-      data={{
-        labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-        datasets: [
-          {
-            label: 'Math',
-            data: [3, 6, 2, 8, 5, 8, 7, 5, 3, 6, 9],
-            backgroundColor: 'red'
-          }
-        ]
-      }}
-      return
-      height={600}
-      width={800}
-      options={{ maintainAspectRatio: false }}
-    />
-  </div>;
+  return (
+    <div class="container">
+      <Bar
+        data={{
+          labels: [
+            '0',
+            '0.2',
+            '0.4',
+            '0.6',
+            '0.8',
+            '1',
+            '1.2',
+            '1.4',
+            '1.6',
+            '1.8',
+            '2',
+            '2.2',
+            '2.4',
+            '2.6',
+            '2.8',
+            '3',
+            '3.2',
+            '3.4',
+            '3.6',
+            '3.8',
+            '4',
+            '4.2',
+            '4.4',
+            '4.6',
+            '4.8',
+            '5',
+            '5.2',
+            '5.4',
+            '5.6',
+            '5.8',
+            '6',
+            '6.2',
+            '6.4',
+            '6.6',
+            '6.8',
+            '7',
+            '7.2',
+            '7.4',
+            '7.6',
+            '7.8',
+            '8',
+            '8.2',
+            '8.4',
+            '8.6',
+            '8.8',
+            '9',
+            '9.2',
+            '9.4',
+            '9.6',
+            '9.8',
+            '10'
+          ],
+          datasets: [
+            {
+              label: 'Toán',
+              data: [3, 6, 2, 8, 5, 8, 7, 5, 3, 6, 9],
+              backgroundColor: 'red'
+            }
+          ]
+        }}
+        height={600}
+        width={600}
+        options={{ maintainAspectRatio: false }}
+      />
+    </div>
+  );
 }
 
 export default Statistics;
