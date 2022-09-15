b534b0bc2ee11eeebed2dc365fb5efcbc10e293c
Quyen2010 <qn018231@gmail.com>
2022-09-15 07:06:58 +0700

    comment

--- client/src/components/satistics/satistics.jsx
+++ client/src/components/satistics/satistics.jsx
@@ -1,15 +1,48 @@
-function Statistics() {
-  return(
-    <div>
-      <h4>This is statistics component</h4>
-      <img
-        src="https://scontent.fsgn2-2.fna.fbcdn.net/v/t1.15752-9/296162066_999828024020620_5212260318443284224_n.png?_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=MfkxWqHyejMAX-E5hsa&_nc_ht=scontent.fsgn2-2.fna&oh=03_AVLWsXMdJ64qo_Q6i4Q2357GdYeYDUnn7iKuXvy_b5j3eg&oe=633CBC18"
-        alt="meop"
-        width="200"
-        height="250"
-      />
-    </div>
-  );
-}
 
+import React from 'react';
+import  './satistics.scss';
+// let xml = new XMLHttpRequest();
+// // let url = "http://localhost:3000/statistics/js/....data.json";
+// xml.open("GET",ulr,true);
+// xml.send();
+// xml.onreadystatechange = function(){
+//     if(this.readyState == 4 && this.status ==200){
+//         let data = JSON.parse(this.responseText);
+//         console.log(data);
+//     }
+// }
+
+const ctx = document.getElementById('canvas').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Thong ke',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+            ],
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+function Statistics() {
+  return (
+    <div className="container">
+    <canvas id="canvas" width="250" height="200"></canvas>
+    <h1>BIỂU ĐỒ PHÂN BỐ ĐIỂM THI TỐT NGHIỆP THPT MÔN NGỮ VĂN NĂM 2021</h1>
+    </div>
+  )
+}
 export default Statistics;
+
+
+
+
