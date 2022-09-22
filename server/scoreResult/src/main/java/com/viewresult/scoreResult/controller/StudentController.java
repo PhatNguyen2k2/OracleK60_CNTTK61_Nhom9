c3d0e87f9bfdd684bb8c5e9190be7d9291eb122a
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2022-09-22 21:33:35 +0700

    create api marks and counts of statistics (done)

--- server/scoreResult/src/main/java/com/viewresult/scoreResult/controller/StudentController.java
+++ server/scoreResult/src/main/java/com/viewresult/scoreResult/controller/StudentController.java
@@ -3,16 +3,14 @@ package com.viewresult.scoreResult.controller;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 import com.viewresult.scoreResult.model.Student;
 import com.viewresult.scoreResult.repository.StudentRepository;
@@ -34,11 +32,21 @@ public class StudentController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-	@GetMapping("/statistics/maths")
-	@Query("Select count(SBD) From DATA Where TOÁN = 0")
-	public ResponseEntity<Long> countStudent(long doc){
-		if (doc != 0) {
-			return new ResponseEntity<>(doc, HttpStatus.OK);
+	@GetMapping("/statistics/marks")
+	public ResponseEntity<String> get_points(@RequestParam(name = "subject") String subject){
+		String arr = studentRepository.marks(subject);
+		String newArr = arr.substring(1, arr.length());
+		if (!newArr.isEmpty()) {
+			return new ResponseEntity<>(newArr, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	@GetMapping("/statistics/counts")
+	public ResponseEntity<String> get_subject_points(@RequestParam(name = "subject") String subject){
+		String arr = studentRepository.count(subject);
+		if (!arr.isEmpty()) {
+			return new ResponseEntity<>(arr, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
